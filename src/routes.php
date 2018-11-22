6e9f1040f6713c6b415d561ca2c9a203f2505732
Antony Padayattil <antony.p@reportgarden.com>
2018-11-22 11:03:07 +0530

    remove requests lib

--- src/routes.php
+++ src/routes.php
@@ -5,10 +5,17 @@ use Slim\Http\Response;
 
 // Routes
 
-$app->get('/[{name}]', function (Request $request, Response $response, array $args) {
+$app->get('/', function (Request $request, Response $response, array $args) {
     // Sample log message
     $this->logger->info("Slim-Skeleton '/' route");
 
     // Render index view
     return $this->renderer->render($response, 'index.phtml', $args);
+});
+
+$app->get('/search', function (Request $request, Response $response, array $args) {
+    $searchQuery = $request->getQueryParam('q', '');
+    $responseJSON = array('searchQuery' => $searchQuery);
+    
+    return $response->withJson($responseJSON);
 });
